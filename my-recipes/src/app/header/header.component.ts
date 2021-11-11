c53827687c844a70987e82bd22ad716cddc2232f
ivan-angjelovski <62544677+ivan-angjelovski@users.noreply.github.com>
2021-11-11 11:34:45 +0100

    Header layed out

--- my-recipes/src/app/header/header.component.ts
+++ my-recipes/src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  collapsed:boolean = true;
   constructor() { }
 
   ngOnInit(): void {
